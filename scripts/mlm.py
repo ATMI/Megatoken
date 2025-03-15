a442122a463c4974c88a02de5d8d4dd785e573d8
t <t.brayko@innopolis.university>
2025-03-16 00:23:01 +0300

    Expand Batch and Log solution

--- scripts/mlm.py
+++ scripts/mlm.py
@@ -1,4 +1,6 @@
 import argparse
+import time
+from functools import partial
 from pathlib import Path
 from typing import Dict, Tuple
 
@@ -16,21 +18,21 @@ from pipeline.train import train
 from utils.config import load_config
 
 
-class MLMBatch(Batch):
+class MaskModelBatch(Batch):
 
 	def __init__(
 		self,
 		batch,
-		pad: int,
-		mask_token_id: int,
-		causal_mask: bool,
+		pad_index: int,
+		mask_index: int,
+		causal_mask: bool = True,
 		mask_prob: float = 0.15,
 		ignore_index: int = -100,
 	):
-		self.x, self.pad_mask = self.collate(batch)
 
-		self.pad_index = pad
-		self.mask_token_id = mask_token_id
+		self.batch, self.pad_mask = self.collate(batch)
+		self.pad_index = pad_index
+		self.mask_token_id = mask_index
 		self.ignore_index = ignore_index
 		self.mask_prob = mask_prob
 		self.causal_mask = causal_mask
@@ -60,28 +62,29 @@ class MLMBatch(Batch):
 		x_lens = self.pad_mask.size(1) - self.pad_mask.sum(dim=1)
 		num_to_mask = (self.mask_prob * x_lens).int().clamp(min=1)
 
-		mask = torch.zeros_like(self.x, dtype=torch.bool)
+		mask = torch.zeros_like(self.batch, dtype=torch.bool)
 
-		for i in range(self.x.size(0)):
+		for i in range(self.batch.size(0)):
 			candidates = torch.where(~self.pad_mask[i])[0]
 			perm = torch.randperm(x_lens[i])
 			selected = candidates[perm[:num_to_mask[i]]]
 			mask[i, selected] = True
 
-		masked_input = self.x.clone()
+		masked_input = self.batch.clone()
 		masked_input[mask] = self.mask_token_id
 
-		labels = torch.full_like(self.x, fill_value=self.ignore_index)  # -100 = ignore index for CrossEntropyLoss
-		labels[mask] = self.x[mask]
+		labels = torch.full_like(self.batch, fill_value=self.ignore_index)  # -100 = ignore index for CrossEntropyLoss
+		labels[mask] = self.batch[mask]
 
 		return masked_input, labels
 
+	@property
 	def x(self) -> Dict[str, any]:
 		self.masked_x, self.label = self.masking()
 
 		attn_mask = None
 		if self.causal_mask:
-			attn_mask = nn.Transformer.generate_square_subsequent_mask(self.x.size(1), dtype=torch.bool)
+			attn_mask = nn.Transformer.generate_square_subsequent_mask(self.batch.size(1), dtype=torch.bool)
 
 		inputs = {
 			"x": self.masked_x,
@@ -91,8 +94,10 @@ class MLMBatch(Batch):
 
 		return inputs
 
+	@property
 	def y(self) -> torch.Tensor:
 		"""
+		Shape: [batch_size * seq_len]
 		:return: RETURN TENSOR FOR CROSS-ENTROPY-LOSS
 		"""
 		flat_label = self.label.flatten()
@@ -132,20 +137,21 @@ class MaskModelCheckpoint(Checkpoint):
 
 		return step.is_last or cond
 
-class MLMLog(Log):
+
+class MaskModelLog(Log):
 
 	def info(self, step: Step) -> Dict[str, any]:
 		acc, acc_ = self.accuracy(step.result)
 		ppl = self.perplexity(step.result.loss)
 		loss_ = self.topk_loss(step.result.loss)
 
-		# TODO: Any other log values?
+		# TODO: Fix ratios
 		logs = {
 			"loss": step.result.loss,
 			"loss@K": loss_,
 			"acc": acc,
 			"acc@K": acc_,
-			"ratio": step.result.pred.ratio,
+			"ratio": step.result.pred.ratios.mean(),
 			"PPL": ppl,
 		}
 
@@ -153,8 +159,9 @@ class MLMLog(Log):
 
 	def accuracy(self, result: StepResult) -> Tuple[float, float]:
 		out = result.pred
-		label = result.batch.label  # TODO: access labels with shapes | reshape label here (bad idea)
-		ignore_index = result.batch.ignore_index  # TODO: access ignore index
+		#TODO: fix access for label and ignore_index
+		label = result.batch.label
+		ignore_index = result.batch.ignore_index
 
 		y_pred = out.argmax(dim=-1)
 		correct = (y_pred == label).sum().item()
@@ -189,7 +196,7 @@ class MLMLog(Log):
 		loss_avg = self.losses_sum / len(self.losses)
 		return loss_avg
 
-	def perplexity(self, loss: torch.Tensor) -> float:
+	def perplexity(self, loss) -> float:
 		return torch.exp(loss).item()
 
 
@@ -197,6 +204,7 @@ def main():
 	args = argparse.ArgumentParser()
 	args.add_argument("config", type=Path, required=True)
 	args.add_argument("output", type=Path, required=True)
+	args.add_argument("tokenizer", type=str, default="google-bert/bert-base-uncased")
 	args = args.parse_args()
 
 	if args.output.exists():
@@ -208,18 +216,28 @@ def main():
 	dataset = {}
 	model = nn.Module()
 
+	tokenizer_config = getattr(config.Tokenizer, args.tokenizer)
+	#TODO provide other values in config file!
+	batch_kwargs = {
+		"pad_index": tokenizer_config.pad,
+		"mask_index": tokenizer_config.mask,
+		"causal_mask": True,
+		"mask_prob": 0.15,
+		"ignore_index": -100,
+	}
+
 	train_loader = data.DataLoader(
 		dataset=dataset["train"],
 		batch_size=config.train.batch,
 		shuffle=True,
-		collate_fn=MLMBatch,
+		collate_fn=partial(MaskModelBatch, **batch_kwargs),
 	)
 
 	test_loader = data.DataLoader(
 		dataset=dataset["test"],
 		batch_size=config.test.batch,
 		shuffle=False,
-		collate_fn=MLMBatch,
+		collate_fn=partial(MaskModelBatch, **batch_kwargs),
 	)
 
 	criterion = nn.CrossEntropyLoss()
@@ -231,8 +249,8 @@ def main():
 		num_training_steps=config.epochs * len(train_loader),
 	)
 
-	log = MLMLog(args.output)
-	checkpoint = MLMCheckpoint(args.output)
+	log = MaskModelLog(args.output)
+	checkpoint = MaskModelCheckpoint(args.output)
 
 	train(
 		epochs=config.epochs,
