Fixed inputs calculation in the MaskModelBatch

model/transformer.py
@@ -79,7 +79,7 @@ class GatedTransformer(nn.Module):
 	def forward(
 		self,
 		x: Tensor, x_pad: Tensor,
-		y: Tensor, y_pad: Tensor,
+		z: Tensor, z_pad: Tensor,
 	) -> Tuple[Tensor, Tensor, List[float]]:
 		ratios = []
 
@@ -99,13 +99,14 @@ class GatedTransformer(nn.Module):
 
 			e, e_pad = t, t_pad
 
-		y = self.embedding(y)
-		y = self.positional(y)
+		z = self.embedding(z)
+		z = self.positional(z)
 
+		y_pad = z_pad
 		y = self.decoder(
-			y, e,
+			z, e,
 			None, None,
-			y_pad, e_pad,
+			z_pad, e_pad,
 			False, False,
 		)
 

pipeline/mlm/batch.py
@@ -1,7 +1,7 @@
-from typing import Dict
+from typing import Dict, Tuple
 
 import torch
-from torch import nn
+from torch import nn, Tensor
 from torch.nn.utils import rnn
 
 from pipeline.base.batch import Batch
@@ -11,27 +11,38 @@ class MaskModelBatch(Batch):
 
 	def __init__(
 		self,
-		batch,
-		pad_index: int,
-		mask_index: int,
-		causal_mask: bool = True,
-		mask_prob: float = 0.15,
-		ignore_index: int = -100,
+		batch: any,
+
+		pad_token: int,
+		mask_token: int,
+		ignore_token: int,
+		prob: float,
+
+		causal: bool,
 	):
-		self.batch, self.pad_mask = self.collate(batch)
-		self.pad_index = pad_index
-		self.mask_token_id = mask_index
-		self.ignore_index = ignore_index
-		self.mask_prob = mask_prob
-		self.causal_mask = causal_mask
+		super(MaskModelBatch, self).__init__(batch)
 
-		self.masked_x = None
-		self.label = None
-		self.attn_mask = None
+		x, pad = self.collate(batch, pad_token)
+		z, y = self.masking(x, pad, mask_token, ignore_token, prob)
 
-	def collate(self, batch):
+		if causal:
+			attn = nn.Transformer.generate_square_subsequent_mask(
+				x.size(1),
+				torch.bool,
+			)
+			raise NotImplementedError("Attn is not used by the model for now")
+		else:
+			attn = None
+
+		self.x_, self.pad = x, pad
+		self.z_ = z
+		self.y_ = y
+		self.attn = attn
+
+	@staticmethod
+	def collate(batch, pad_token):
 		x = [torch.tensor(x["tokens"]) for x in batch]
-		x = rnn.pad_sequence(x, batch_first=True, padding_value=self.pad_index)
+		x = rnn.pad_sequence(x, batch_first=True, padding_value=pad_token)
 
 		batch_size = x.size(0)
 		seq_len = x.size(1)
@@ -42,6 +53,7 @@ class MaskModelBatch(Batch):
 			dtype=torch.bool,
 			device=x.device
 		)
+
 		for i in range(batch_size):
 			tokens = batch[i]["tokens"]
 			length = len(tokens)
@@ -50,49 +62,44 @@ class MaskModelBatch(Batch):
 		return x, x_pad
 
 	# TODO: make wider masks and mode
-	def masking(self):
-		x_lens = self.pad_mask.size(1) - self.pad_mask.sum(dim=1)
-		num_to_mask = (self.mask_prob * x_lens).int().clamp(min=1)
+	@staticmethod
+	def masking(
+		x: Tensor,
+		x_pad: Tensor,
+		mask_token: int,
+		ignore_token: int,
+		prob: float,
+	) -> Tuple[Tensor, Tensor]:
+		x_len = x_pad.size(1) - x_pad.sum(dim=1)
 
-		mask = torch.zeros_like(self.batch, dtype=torch.bool)
+		num_to_mask = (prob * x_len).int().clamp(min=1)
+		mask = torch.zeros_like(x, dtype=torch.bool)
 
-		for i in range(self.batch.size(0)):
-			candidates = torch.where(~self.pad_mask[i])[0]
-			perm = torch.randperm(x_lens[i])
+		for i in range(x.size(0)):
+			candidates = torch.where(~x_pad[i])[0]
+			perm = torch.randperm(x_len[i])
 			selected = candidates[perm[:num_to_mask[i]]]
 			mask[i, selected] = True
 
-		masked_input = self.batch.clone()
-		masked_input[mask] = self.mask_token_id
+		y = x.clone()
+		y[mask] = mask_token
 
-		labels = torch.full_like(
-			self.batch,
-			fill_value=self.ignore_index
-		)  # -100 = ignore index for CrossEntropyLoss
-		labels[mask] = self.batch[mask]
+		# -100 = ignore index for CrossEntropyLoss
+		labels = torch.full_like(x, fill_value=ignore_token)
+		labels[mask] = x[mask]
 
-		return masked_input, labels
+		return y, labels
 
 	@property
 	def x(self) -> Dict[str, any]:
-		self.masked_x, self.label = self.masking()
+		x = {
+			"x": self.x_,
+			"x_pad": self.pad,
 
-		attn_mask = None
-		if self.causal_mask:
-			attn_mask = nn.Transformer.generate_square_subsequent_mask(
-				self.batch.size(1),
-				dtype=torch.bool
-			)
-
-		# TODO: fix the input names
-		inputs = {
-			"x": self.batch,
-			"mask_x": self.masked_x,
-			"pad": self.pad_mask,
-			"attn": attn_mask
+			"z": self.z_,
+			"z_pad": self.pad,
 		}
-
-		return inputs
+		return x
 
 	@property
 	def y(self) -> torch.Tensor:
@@ -100,5 +107,5 @@ class MaskModelBatch(Batch):
 		Shape: [batch_size * seq_len]
 		:return: RETURN TENSOR FOR CROSS-ENTROPY-LOSS
 		"""
-		flat_label = self.label.flatten()
-		return flat_label
+		y = self.y_.flatten()
+		return y

pipeline/mlm/model.py
@@ -19,9 +19,9 @@ class MaskModel(nn.Module):
 		x: torch.Tensor,
 		x_pad: torch.Tensor,
 
-		y: torch.Tensor,
-		y_pad: torch.Tensor,
+		z: torch.Tensor,
+		z_pad: torch.Tensor,
 	) -> any:
-		y, y_pad, ratio = self.transformer(x, x_pad, y, y_pad)
+		y, y_pad, ratio = self.transformer(x, x_pad, z, z_pad)
 		y = self.classifier(y)
 		return y, y_pad, ratio

train.py
@@ -7,7 +7,7 @@ from torch.utils import data
 from transformers import get_scheduler
 
 from pipeline.base.train import train
-# from scripts.dataset import prepare_dataset
+from scripts.dataset import prepare_dataset
 from pipeline.mlm.batch import MaskModelBatch
 from pipeline.mlm.checkpoint import MaskModelCheckpoint
 from pipeline.mlm.log import MaskModelLog
@@ -21,9 +21,9 @@ def main():
 	args.add_argument("output", type=Path)
 	args = args.parse_args()
 
-	if args.output.exists():
-		print("Output file already exists. Aborting.")
-		exit(1)
+	# if args.output.exists():
+	# 	print("Output file already exists. Aborting.")
+	# 	exit(1)
 
 	config = load_config(args.config)
 
@@ -46,26 +46,27 @@ def main():
 		tokenized_col=config.dataset.text_col,
 	)
 
-	batch_kwargs = {
-		"pad_index": config.tokenizer.pad,
-		"mask_index": config.tokenizer.mask,
-		"causal_mask": True,  # TODO: False?
-		"mask_prob": 0.15,
-		"ignore_index": -100,
-	}
+	batch_factory = partial(
+		MaskModelBatch,
+		pad_token=config.tokenizer.pad,
+		mask_token=config.tokenizer.mask,
+		ignore_token=-100,
+		prob=0.15,
+		causal=False,
+	)
 
 	train_loader = data.DataLoader(
 		dataset=dataset["train"],
 		batch_size=config.train.batch,
 		shuffle=True,
-		collate_fn=partial(MaskModelBatch, **batch_kwargs),
+		collate_fn=batch_factory,
 	)
 
 	test_loader = data.DataLoader(
 		dataset=dataset["test"],
 		batch_size=config.test.batch,
 		shuffle=False,
-		collate_fn=partial(MaskModelBatch, **batch_kwargs),
+		collate_fn=batch_factory,
 	)
 
 	criterion = nn.CrossEntropyLoss()