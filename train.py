Fixed the mistakes with volume loss calculation

train.py
@@ -25,6 +25,10 @@ def main():
 	model = model.to(device)
 	optimizer = optim.Adam(model.parameters(), Config.lr)
 
+	# init = torch.load("checkpoint.pth", map_location=device, weights_only=True)
+	# model.load_state_dict(init["model"])
+	# optimizer.load_state_dict(init["optimizer"])
+
 	step_num = len(train_loader)
 	bar = tqdm(total=step_num)
 
@@ -66,9 +70,7 @@ def main():
 		)
 
 		input_length = batch.pad_mask.sum(dim=1)
-		input_length = input_length.unsqueeze(1)
-
-		loss_volume = (result.volume[:, -1] / input_length).mean()
+		loss_volume = (result.volume[:, -1] / input_length - Config.volume_threshold).clamp(min=0).mean()
 		loss_class = fn.cross_entropy(result.logits.flatten(0, 1), batch.labels.flatten())
 		loss = loss_class + loss_volume
 