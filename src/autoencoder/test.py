e35cdd4cc048cb29a2bb41b87e51bfc3a8289074
Artemii Miasoedov <artem.myasoedov@gmail.com>
2025-04-29 12:46:17 +0300

    Tested the model trained with the original loss

--- src/autoencoder/test.py
+++ src/autoencoder/test.py
@@ -1,5 +1,5 @@
 import torch
-from transformers import AutoTokenizer, T5ForConditionalGeneration, AutoConfig
+from transformers import AutoTokenizer
 
 from .model import AutoEncoder, AutoEncoderConfig
 from ..util.prepare import prepare_random, prepare_device
@@ -14,15 +14,22 @@ def main():
 
 	config = AutoEncoderConfig.from_pretrained(model_name)
 	model = AutoEncoder.from_pretrained(model_name, config=config)
+	model.eval()
 	model = model.to(device)
 
-	checkpoint = torch.load("autoencoder_00_14000.pth", map_location=device, weights_only=True)
+	checkpoint = "checkpoint/65586150dd2ce3eba3172ea837b748286e277200/autoencoder_00_34253.pth"
+	checkpoint = torch.load(checkpoint, map_location=device, weights_only=True)
 	model.load_state_dict(checkpoint["model"])
 
 	inputs = [
 		"""(CNN) -- A truck-bomb explosion has killed at least 13 people in the capital of Afghanistan's Logar province, a provincial spokesman said. The attack Saturday night killed four civilians and several militants, including members of the Pakistani Taliban and an affiliated group, said Din Mohammad Darwis, the spokesman. It happened in Pul e Alam, in central Afghanistan.""",
 		"""(CNN) One person was killed after more than 100 cars piled up on Interstate 94 near Kalamazoo, Michigan, state police said Friday. About 20 more were injured, with 10 of those injuries "more serious" in nature, Michigan State Police Officer Shane Criger said. One of the tractor-trailers involved in the wreck was carrying fireworks that can be seen on video detonating.""",
 	]
+
+	print("Inputs:")
+	for sample in inputs:
+		print(sample)
+	print()
 
 	inputs = tokenizer(inputs, return_tensors="pt", padding=True)
 	inputs["use_cache"] = False
@@ -40,8 +47,9 @@ def main():
 		skip_special_tokens=True,
 	)
 
-	for output in outputs:
-		print(output)
+	print("Outputs:")
+	for sample in outputs:
+		print(sample)
 
 
 if __name__ == "__main__":
